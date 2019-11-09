aed8fbc1c88cda8f5e44b4cef46c66468607664d
Thiago Navarro <t_61327@cpqd.com.br>
2019-11-09 19:57:39 -0300

    Updates svm project

--- IA006-EFC03/svm.py
+++ IA006-EFC03/svm.py
@@ -19,6 +19,11 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, svm
 import scipy.io as sio
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.model_selection import PredefinedSplit
+from sklearn import preprocessing
 # %matplotlib inline
 
 # %%
@@ -27,74 +32,116 @@ dados_treino = sio.loadmat('dados_treinamento.mat')
 dados_valid = sio.loadmat('dados_val.mat')
 
 X_test = np.array(dados_teste["Xt"])
-y_test =  np.array(dados_teste["yt"])
+y_test =  np.array(dados_teste["yt"]).astype(np.float)
 X_train =  np.array(dados_treino["X"])
-y_train =  np.array(dados_treino["y"])
-x_valid =  np.array(dados_valid["Xval"])
-y_valid =  np.array(dados_valid["yval"])
+y_train =  np.array(dados_treino["y"]).astype(np.float)
+X_valid =  np.array(dados_valid["Xval"])
+y_valid =  np.array(dados_valid["yval"]).astype(np.float)
 
-iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features. We could
-                      # avoid this ugly slicing by using a two-dim dataset
-Y = iris.target
+print(np.mean(X_train, axis=0))
 
-def my_kernel(X, Y):
-    """
-    We create a custom kernel:
-
-                 (2  0)
-    k(X, Y) = X  (    ) Y.T
-                 (0  1)
-    """
-    M = np.array([[2, 0], [0, 1.0]])
-    return np.dot(np.dot(X, M), Y.T)
-
-
-h = .02  # step size in the mesh
-
-# we create an instance of SVM and fit out data.
-#clf = svm.SVC(kernel=my_kernel)
 
 # %%
-#clf = svm.SVC(gamma='scale')
-clf = svm.SVC(gamma=100)
-clf.fit(X, Y.ravel())
+print(np.mean(X_train, axis=0))
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+print(scaler.mean_)
+print(scaler.var_)
 
-# Plot the decision boundary. For that, we will assign a color to each
-# point in the mesh [x_min, x_max]x[y_min, y_max].
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-#plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
-plt.pcolormesh(xx, yy, Z, cmap=plt.cm.bwr)
-
-# Plot also the training points
-#plt.scatter(X[:, 0], X[:, 1], c=Y.ravel(), cmap=plt.cm.bwr, edgecolors='k', s=10)
-#plt.scatter(X[:, 0], X[:, 1], c=Y.ravel(), cmap=plt.cm.bwr, s=5, edgecolors='k')
-#plt.scatter(X[:, 0], X[:, 1], c=Y.ravel(), cmap=plt.cm.bwr, s=5, edgecolors='k')
-plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=1,
-                facecolors='none', zorder=10, edgecolors='k')
-#print(clf.support_vectors_[:, 0].shape[0], X[:, 0].shape[0])
-plt.title('2-Class classification using Support Vector Machine')
-plt.axis('tight')
+# %%
+# An "interface" to matplotlib.axes.Axes.hist() method
+n, bins, patches = plt.hist(y_train, bins=20, color='#0504aa',
+                            alpha=0.7)
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('y_test')
+plt.ylabel('Frequency')
+plt.title('Y Train Histogram')
 plt.show()
 
-# Plot also the training points
-#plt.scatter(X[:, 0], X[:, 1], c=Y.ravel(), cmap=plt.cm.bwr, s=1)
-#plt.show()
+# An "interface" to matplotlib.axes.Axes.hist() method
+n, bins, patches = plt.hist(X_train, bins=20,alpha=0.7)
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('X_train')
+plt.ylabel('Frequency')
+plt.title('Y train Histogram')
+plt.show()
+
 
 # %%
-gamma_array = np.arange(0.1, 100, 0.1)
+    
+def svm_fit_plot_report(X_train, y_train, X_valid, y_valid, gamma_array, penality_array):
+    scores = ['accuracy']    
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+        parameters = {'C':penality_array, 'gamma':gamma_array}
 
-for gamma in gamma_array:
-    clf = svm.SVC(gamma=gamma)
-    this_scores = cross_val_score(clf, X, Y.ravel(), cv=5, n_jobs=-1)
+        # The indices which have the value -1 will be kept in train.
+        train_indices = np.full((len(X_train),), -1, dtype=int)
+
+        # The indices which have zero or positive values, will be kept in test
+        test_indices = np.full((len(X_valid),), 0, dtype=int)
+        test_fold = np.append(train_indices, test_indices) 
+
+        ps = PredefinedSplit(test_fold)
+        
+        svc = svm.SVC()
+        clf = GridSearchCV(svc, parameters, cv=ps, n_jobs=-1)
+        
+        X = np.concatenate((X_train, X_valid))
+        y = np.concatenate((y_train.ravel(), y_valid.ravel()))
+        print(len(X_train))
+        print(len(X))
+        clf.fit(X, y)
+    
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r"
+                  % (mean, std * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print()
+    
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+        
+        h = .02  # step size in the mesh
+        
+        x_min, x_max = X_train[:, 0].min(), X_train[:, 0].max()
+        y_min, y_max = X_train[:, 1].min(), X_train[:, 1].max()
+        
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.pcolormesh(xx, yy, Z, cmap=plt.cm.bwr)
+
+        plt.title('2-Class classification using Support Vector Machine')
+        plt.axis('tight')
+        plt.show()
+
 
 # %%
-this_scores
+svm_fit_plot_report(X_train, y_train.ravel(),
+                       X_valid, y_valid.ravel(),
+                       np.geomspace(0.001, 1000, 7), #gamma
+                       np.geomspace(0.001, 1000, 7)) #penality
 
 # %%
